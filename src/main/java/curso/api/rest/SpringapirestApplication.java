af8b4ea2d73a3d42f54d0e9f9055e980d5b4c8d1
Pelegrino <pelegrino@gmail.com>
2023-09-05 15:11:35 -0300

    Initial commit

---
+++ src/main/java/curso/api/rest/SpringapirestApplication.java
@@ -0,0 +1,13 @@
+package curso.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapirestApplication.class, args);
+	}
+
+}
