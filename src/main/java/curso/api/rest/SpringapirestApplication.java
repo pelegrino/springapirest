16223d004f241ef6a27c0c7cf380eb9e30d2df3a
Pelegrino <pelegrino@gmail.com>
2023-09-14 09:42:28 -0300

    Spring Security - Configurando ROLE

--- src/main/java/curso/api/rest/SpringapirestApplication.java
+++ src/main/java/curso/api/rest/SpringapirestApplication.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"curso.api.rest.model"})
@@ -18,10 +20,18 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
-public class SpringapirestApplication {
+public class SpringapirestApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringapirestApplication.class, args);
+	}
+	
+	//Mapeamento Global de Acesso
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/usuario/**") //Onde pode ser acessado
+		.allowedMethods("*") //O que pode ser acessado
+		.allowedOrigins("*"); //Quem pode acessar
 	}
 
 }
--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import curso.api.rest.model.Usuario;
 import curso.api.rest.repository.UsuarioRepository;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
