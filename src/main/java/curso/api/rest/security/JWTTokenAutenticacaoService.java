5916e6c31552bcd89e2884aa81dbaf3fb34b9455
Pelegrino <pelegrino@gmail.com>
2023-09-16 16:40:35 -0300

    Create JWTTokenAutenticacaoService.java

---
+++ src/main/java/curso/api/rest/security/JWTTokenAutenticacaoService.java
@@ -0,0 +1,13 @@
+package curso.api.rest.security;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+@Component
+public class JWTTokenAutenticacaoService {
+
+	@
+	private static final long EXPIRATION_TIME = 172800000;
+	
+}
