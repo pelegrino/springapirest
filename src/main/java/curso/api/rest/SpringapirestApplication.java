39efecd5a33a84188dd825e1020ee198b8609f3d
Pelegrino <pelegrino@gmail.com>
2023-10-15 19:38:24 -0300

    Atualização do suporte

--- src/main/java/curso/api/rest/SpringapirestApplication.java
+++ src/main/java/curso/api/rest/SpringapirestApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,16 +24,6 @@ public class SpringapirestApplication implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringapirestApplication.class, args);
 		// System.out.println(new BCryptPasswordEncoder().encode("1234"));
-	}
-
-	// Mapeamento Global de Acesso
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**") // Onde pode ser acessado
-				.allowedMethods("*")
-				.allowedOrigins("*")
-				.allowCredentials(true);
-
 	}
 
 }
--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -14,6 +14,7 @@ import org.springframework.cache.annotation.CachePut;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import curso.api.rest.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping(value = "/usuario")
+@CrossOrigin(origins = "*")
 public class IndexController {
 	
 	@Autowired
